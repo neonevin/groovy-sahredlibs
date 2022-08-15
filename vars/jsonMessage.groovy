49317630ff6ba68326308d775bf3a7d55cc2fd9f
Nevin <nevinprasannan@gmail.com>
2022-08-15 19:16:42 +1000

    Update jsonMessage.groovy

--- vars/jsonMessage.groovy
+++ vars/jsonMessage.groovy
@@ -11,13 +11,18 @@ def jsonParseData(jsonObj) {
 def buildMessage() {
 def builder = new JsonBuilder()
 builder.book {
-    title 'Head First Java'
+/*    title 'Head First Java'
     publisher 'Orielly'
     author 'Kathy Sierra', 'Bert Bates'
     year '2005'
     currency 'USD'
     price 44.95
     format 'pdf', 'print'
+    */
+    PHI_DOMAIN_ID 'HR'
+    PHI_CR_NUM 'CR000012'
+    DEL_JIRA_STATUS 'TEST'
+    PHI_ASSIGN_TO 'DTTLNARESH'
     }
 }
 
@@ -27,3 +32,15 @@ def call() {
     println ""
     println builder.toString()
 }
+/*
+def buildMessage() {
+    JsonBuilder builder = new JsonBuilder()
+    builder.CR_TASK_NEXT_STEP_REQ {
+            PHI_DOMAIN_ID 'HR'
+            PHI_CR_NUM 'CR000012'
+            DEL_JIRA_STATUS 'TEST'
+            PHI_ASSIGN_TO 'DTTLNARESH'
+    }
+
+}
+*/

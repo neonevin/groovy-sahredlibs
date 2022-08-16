67d45380912b8f0ba6f651ff2d9060643753d38c
Nevin <nevinprasannan@gmail.com>
2022-08-16 12:51:33 +1000

    Update jsonMessage.groovy

--- vars/jsonMessage.groovy
+++ vars/jsonMessage.groovy
@@ -11,20 +11,12 @@ def jsonParseData(jsonObj) {
 def buildMessage() {
 def builder = new JsonBuilder()
 /*builder.CR_TASK_NEXT_STEP_REQ {
-/*    title 'Head First Java'
-    publisher 'Orielly'
-    author 'Kathy Sierra', 'Bert Bates'
-    year '2005'
-    currency 'USD'
-    price 44.95
-    format 'pdf', 'print'
-    */
     'PHI_DOMAIN_ID' 'HR'
     'PHI_CR_NUM' 'CR000012'
     'DEL_JIRA_STATUS' 'TEST'
     'PHI_ASSIGN_TO' 'DTTLNARESH'
     }*/
-    builder.book {
+builder.book {
     title 'Head First Java'
     publisher 'Orielly'
     author 'Kathy Sierra', 'Bert Bates'

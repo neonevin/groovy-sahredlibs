6dfa4fc0c8baa26fde9e341e0899d43f458dae29
neonevin <nevinprasannan@gmail.com>
2022-09-13 17:27:07 +1000

    change to call

--- vars/PhireRequest.groovy
+++ vars/PhireRequest.groovy
@@ -131,8 +131,8 @@ def call(LinkedHashMap args, String userid, String password, String phireTktId, 
     //sendMessage(builder,userid,password)
 
     //def response = args.jenkinsWorkflowScript.invokeMethod 'JiraAddComment' [comment: "Test message", idOrKey: tktId, site: 'JiraSSL']
-    def response = args.jenkinsWorkflowScript.invokeMethod 'jiraAddComment', [comment: jiraComment, idOrKey: tktId, site: 'JiraSSL']
-
+    //def response = args.jenkinsWorkflowScript.invokeMethod 'jiraAddComment', [comment: jiraComment, idOrKey: tktId, site: 'JiraSSL']
+    return jiraComment
 }
 
 
